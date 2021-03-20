5979108b1667e0dabb9e82a9741e3c5befc51cf7
pratamaandiko <pratamandiko@gmail.com>
2021-03-20 17:15:21 +0700

    add validation and Graph

--- OnlyVANS/OnlyVANS/Form1.Designer.cs
+++ OnlyVANS/OnlyVANS/Form1.Designer.cs
@@ -32,8 +32,10 @@ namespace OnlyVANS
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label9 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
@@ -47,11 +49,7 @@ namespace OnlyVANS
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.label8 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label10 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -101,25 +99,47 @@ namespace OnlyVANS
             this.panel1.Size = new System.Drawing.Size(622, 922);
             this.panel1.TabIndex = 2;
             // 
+            // panel3
+            // 
+            this.panel3.AutoScroll = true;
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.label10);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Location = new System.Drawing.Point(47, 616);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(525, 273);
+            this.panel3.TabIndex = 14;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(3, 41);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 22);
+            this.label10.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 22);
+            this.label8.TabIndex = 12;
+            // 
             // panel2
             // 
             this.panel2.AutoScroll = true;
             this.panel2.BackColor = System.Drawing.Color.Transparent;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel2.Controls.Add(this.label9);
             this.panel2.Location = new System.Drawing.Point(47, 200);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(525, 323);
             this.panel2.TabIndex = 13;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label9.Location = new System.Drawing.Point(7, 9);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(2, 19);
-            this.label9.TabIndex = 0;
             // 
             // label7
             // 
@@ -168,6 +188,11 @@ namespace OnlyVANS
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "V",
+            "C"});
             this.comboBox2.Location = new System.Drawing.Point(267, 584);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 24);
@@ -177,6 +202,10 @@ namespace OnlyVANS
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C"});
             this.comboBox1.Location = new System.Drawing.Point(267, 544);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
@@ -252,38 +281,6 @@ namespace OnlyVANS
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.Transparent;
-            this.label8.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(2, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(0, 22);
-            this.label8.TabIndex = 12;
-            // 
-            // panel3
-            // 
-            this.panel3.AutoScroll = true;
-            this.panel3.BackColor = System.Drawing.Color.Transparent;
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel3.Controls.Add(this.label10);
-            this.panel3.Controls.Add(this.label8);
-            this.panel3.Location = new System.Drawing.Point(47, 616);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(525, 273);
-            this.panel3.TabIndex = 14;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.BackColor = System.Drawing.Color.Transparent;
-            this.label10.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(3, 41);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(0, 22);
-            this.label10.TabIndex = 13;
-            // 
             // onlyvans
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -299,8 +296,6 @@ namespace OnlyVANS
             this.Text = "OnlyVANS";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             this.ResumeLayout(false);
@@ -327,7 +322,6 @@ namespace OnlyVANS
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label8;
--- OnlyVANS/OnlyVANS/Form1.cs
+++ OnlyVANS/OnlyVANS/Form1.cs
@@ -13,19 +13,17 @@ namespace OnlyVANS
     //FORM
     public partial class onlyvans : Form
     {
-        string exploreWith; // var nyimpan pilihan explore with
-        string chooseAccount; // var nyimpan pilihan choose account
-        string algorithm; // pilihan algoritma
-        string filePath; // var buat nyimpan path
+        string exploreWith = ""; // var nyimpan pilihan explore with
+        string chooseAccount = ""; // var nyimpan pilihan choose account
+        string algorithm = ""; // pilihan algoritma
+        string filePath = ""; // var buat nyimpan path
 
         public onlyvans()
         {
-            //gui yang sedang berlangsung disini
             InitializeComponent();
             panel1.BringToFront();
             panel1.Hide();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             //button1 = tombol Start Journey
@@ -43,11 +41,59 @@ namespace OnlyVANS
 
         private void button3_Click(object sender, EventArgs e)
         {
-            //SUBMIT BUTTON
-            //button3
-            label8.Text = "Friends recommendations for " + chooseAccount + ":";
-            //label9 buat gambar graph
-            //label10 buat nampilkan hasil
+            if (filePath == "")
+            {
+                label8.Text = "Message : File belum di input.";
+            }
+            else if(algorithm == "")
+            {
+                label8.Text = "Message : Pilih algoritma terlebih dahulu.";
+            }
+            else if (chooseAccount == "")
+            {
+                label8.Text = "Message : Pilih akun terlebih dahulu.";
+            }
+            else if (exploreWith == "")
+            {
+                label8.Text = "Message : Pilih teman yang ingin dixplore terlebih dahulu.";
+            }
+            else
+            {
+                //SUBMIT BUTTON
+                //button3
+                label8.Text = "Friends recommendations for " + chooseAccount + ":";
+                //label9 buat gambar graph
+                //label10 buat nampilkan hasil
+
+
+                //MENGGAMBAR GRAPH
+                //create a viewer object 
+                Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+                //create a graph object 
+                Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+                //asumsi input 
+                graph.AddEdge("A", "B"); 
+                graph.AddEdge("B", "C");
+                graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+                graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+                graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+                Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
+                c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+                c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+                //bind the graph to the viewer 
+                viewer.Graph = graph;
+                //associate the viewer with the form 
+                panel2.SuspendLayout();
+                viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+                panel2.Controls.Add(viewer);
+                panel2.ResumeLayout();
+
+
+
+            }
+            
+
+
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
